9b62e9a94aba059a23eaf553486182867215e9cc
sy0725 <sunyong0725@naver.com>
2023-08-08 10:42:20 +0900

    0808 수업자료

--- src/FP/chapters/00-programming-paradigm.js
+++ src/FP/chapters/00-programming-paradigm.js
@@ -149,8 +149,12 @@ function createCountUpButton(container, { count : initialCount = 0, step = 1 , m
   }
 
   const handleCountUp = (e) => {
-    count += step;
-    render(count);
+    if(count >= max){
+      alert('더 이상 누를 수가 없습니다');
+    } else {
+      count += step;
+      render(count);
+    }
   }
   
   countUpButton.setAttribute('type', 'button');
@@ -175,7 +179,7 @@ function createCountUpButton(container, { count : initialCount = 0, step = 1 , m
 //  createCountUpButton(demoContainer)
 //  createCountUpButton(demoContainer, { count: 1 })
 //  createCountUpButton(demoContainer, { count: 2 })
-//  createCountUpButton(demoContainer, { count: 3, step : 5 , max : 20})
+//  createCountUpButton(demoContainer, { count: 3, step : 1 , max : 20})
 
 
 
@@ -188,33 +192,53 @@ function createCountUpButton(container, { count : initialCount = 0, step = 1 , m
 
 // 붕어빨틀(생성자함수 : 클래스)
 class CountUpButton {
-  #config;
+  static version = '0.0.1-alpha'
 
-  constructor(userOptions) {
-    this.#config = { ...CountUpButton.defaultProps, ...userOptions };
-    this.init();
+
+  #count
+  // 라이프 사이클 메서드
+  // 생성(constructor) 시점
+  constructor(props){
+  //   console.log({props});
+  this.#count = props.count ?? 0;
   }
 
-  init() {
-    console.log(this.#config);
+  // 랜더
+  render(){
+    const button = document.createElement('button')
+    button.setAttribute('type' , 'button')
+    button.textContent = String(this.#count)
+    return button
   }
 
-  // static field
-  static defaultProps = { 
-    count: 0, 
-    step: 1, 
-  };
-  
+  // 마운트(mount) 시점
+  mount(container){
+    container?.append(this.render())
+  }
+  // 성장(update) 시점
+  // 소멸(unmount) 시점
+  unmount(){
+    console.log('소멸 시점');
+  }
 }
 
 
 // 새로운(new) 붕어빵(객체: 인스턴스) 생성
-const firstCountUp = new CountUpButton({
-  step: 3,
-});
+const firstCountUp = new CountUpButton({count : 1});
+const secondCountUp = new CountUpButton({count : 2});
+const thirdCountUp = new CountUpButton({count : 3});
 
+
+globalThis.firstCountUp = firstCountUp
+
+console.log(firstCountUp);
 
 const demoContainer = document.getElementById('demo');
+
+firstCountUp.mount(demoContainer)
+secondCountUp.mount(demoContainer)
+thirdCountUp.mount(demoContainer)
+
 
 // demoContainer.append(firstCountUp.render());
 
